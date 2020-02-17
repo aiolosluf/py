ec5dfe5f46fe392e86d4f3175c1022ef2c754cad
aiolosluf <aiolos201314@gmail.com>
2020-02-18 10:07:34 +1300

    delete first

--- first.py
+++
@@ -1,9 +0,0 @@
-import random
-import pandas as pd
-import pandas_profiling
-
-data = pd.read_csv('titanic.csv')
-profile = data.profile_report(title='Titanic Dataset')
-profile.to_file(output_file='titanic_report.html')
-
-print('hello')
