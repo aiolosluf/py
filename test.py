ee9186bfc9d7902c336bd680957de974414f6caf
aiolosluf <aiolos201314@gmail.com>
2020-02-18 10:00:15 +1300

    recommit

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import random
 import pandas as pd
 import pandas_profiling
 
-# data = pd.read_csv('titanic.csv')
-# profile = data.profile_report(title='Titanic Dataset')
-# profile.to_file(output_file='titanic_report.html')
+data = pd.read_csv('titanic.csv')
+profile = data.profile_report(title='Titanic Dataset')
+profile.to_file(output_file='titanic_report.html')
 
-for i in range(0,10):
-    print(i)
+# for i in range(0,10):
+#     print(i)
