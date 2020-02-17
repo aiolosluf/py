afc9bbecd2417f9ecfdf00ee37214e935bbd4d0e
aiolosluf <aiolos201314@gmail.com>
2020-02-17 23:44:16 +1300

    test

--- first.py
+++ first.py
@@ -5,3 +5,5 @@ import pandas_profiling
 data = pd.read_csv('titanic.csv')
 profile = data.profile_report(title='Titanic Dataset')
 profile.to_file(output_file='titanic_report.html')
+
+print('hello')
