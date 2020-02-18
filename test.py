42e77db5488cc69ac6ec9e68b7b135a93bceb49b
aiolosluf <aiolos201314@gmail.com>
2020-02-18 23:09:52 +1300

    1

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ data = pd.read_csv('titanic.csv')
 profile = data.profile_report(title='Titanic Dataset')
 profile.to_file(output_file='titanic_report.html')
 
+
 # for i in range(0,10):
 #     print(i)
